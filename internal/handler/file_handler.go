4a1afba345e704f223eba43213dda881fa404bfc
aperezortuno <alexander.perezortuno@gmail.com>
2025-05-20 22:17:17 -0400

    add has header and read csv and load in memory

--- internal/handler/file_handler.go
+++ internal/handler/file_handler.go
@@ -1,20 +1,26 @@
 package handler
 
 import (
-	"bytes"
 	"encoding/csv"
+	"fmt"
 	"github.com/alexperezortuno/go-batch/internal/config"
 	"github.com/alexperezortuno/go-batch/internal/domain"
 	"github.com/alexperezortuno/go-batch/internal/repository"
 	"github.com/alexperezortuno/go-batch/internal/service"
 	"github.com/alexperezortuno/go-batch/internal/utils/logger"
-	"io"
 	"os"
 	"strconv"
 )
 
 func ProcessUserCSV(cfg config.Config, db *repository.Database, logger *logger.Logger) error {
-	data, err := os.ReadFile(cfg.FileProcessing.Paths.UserFile)
+	// Check if the file exists
+	if _, err := os.Stat(cfg.FileProcessing.Paths.UserFile); os.IsNotExist(err) {
+		logger.Error("File does not exist", err)
+		return fmt.Errorf("file does not exist: %w", err)
+	}
+
+	// read the file an load it into memory
+	records, err := readCSVToMemory(cfg.FileProcessing.Paths.UserFile, logger)
 	if err != nil {
 		logger.Error("Error reading file", err)
 		return err
@@ -23,16 +29,15 @@ func ProcessUserCSV(cfg config.Config, db *repository.Database, logger *logger.L
 	repo := &repository.LoaderRepo{DB: db.Db}
 	svc := &service.LoaderService{Repo: repo}
 
-	reader := csv.NewReader(bytes.NewReader(data))
 	var batch []domain.User
 	batchSize := cfg.FileProcessing.BatchSize
 
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
+	for _, record := range records {
+		if cfg.FileProcessing.HasHeader {
+			// Skip the first line if it is a header
+			cfg.FileProcessing.HasHeader = false
+			continue
 		}
-
 		age, _ := strconv.Atoi(record[2])
 		user := domain.User{
 			Name:  record[0],
@@ -53,4 +58,26 @@ func ProcessUserCSV(cfg config.Config, db *repository.Database, logger *logger.L
 		return svc.InsertUserBatch(batch)
 	}
 	return nil
+}
+
+// readCSVToMemory reads an entire CSV file into memory and returns the records as a slice of slices of strings.
+func readCSVToMemory(filePath string, logger *logger.Logger) ([][]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error al abrir el archivo: %w", err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			logger.Error("Error closing file", err)
+		}
+	}(file)
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("error al leer el archivo CSV: %w", err)
+	}
+
+	return records, nil
 }
